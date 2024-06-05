f0f67ef44f1211b7fdd385223353362b5b9abd81
vx_clutch <owestness@gmail.com>
2024-06-04 20:28:14 -0400

    save

---
+++ pkg/build/builder.py
@@ -0,0 +1,10 @@
+class Card():
+    def __init__(self, name, mana, cardType):
+        name = self.name
+        mana = self.mana
+        cardType = self.cardType
+
+def Build(name, mana, cardType):
+    return Card(name, mana, cardType)
+def BuildConfig():
+    return ""
--- pkg/main.py
+++ pkg/main.py
@@ -1,24 +1,2 @@
-class Card:
-    def __init__(self, mana, name, ability):
-        mana = self.mana
-        name = self.name
-        ability = self.ability
+import build.builder as builder
 
-def _ask(q):
-    print(q)
-    i = input(">>> ")
-    j = "nil"
-    match i.lower():
-        case "monster":
-            j = "Monster"
-        case "game":
-            j = "Game"
-    confirmString = f"You picked {j} [Y/n]"
-    confirm = input(confirmString)
-    if confirm.lower() == "y":
-        return True, i
-    if confirm.lower() == "":
-        return True, i
-    return False, ""
-
-err, type = _ask("""Pick a card type:\n\tMonster::0\n\tGame::1""")
